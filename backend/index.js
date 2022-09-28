31e443d04c1672d221ec259a3415eb87dbb5b7bd
AnoneWk <anonewk0@gmail.com>
2022-09-28 14:32:08 +0200

    server

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.use((req, res) => {
+    res.status(404)
+    res.send('Page introuvable')
+})
+
+app.listen(8343, () => {
+    console.log('Serveur enabled on 8343')
+})
