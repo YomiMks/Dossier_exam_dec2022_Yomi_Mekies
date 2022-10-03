084828bd52055a7db573476d62067650b05035ca
AnoneWk <anonewk0@gmail.com>
2022-10-03 13:52:29 +0200

    m

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -30,8 +30,83 @@ const User = sequelize.define('User', {
     timestamps: true
 });
 
+const Partners = sequelize.define('Partners', {
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    enabled: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+}, {
+    timestamps: true
+});
+
+//ejjejee
+
+const Structures = sequelize.define('Structures', {
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    enabled: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    enabled: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+}, {
+    timestamps: true
+});
+
+const Permission = sequelize.define('Permission', {
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    Permission: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+}, {
+    timestamps: true
+});
+
+
 // syncro table
 User.sync()
 // User.sync({ force: true })
 // `sequelize.define` also returns the model
 console.log('dd', User === sequelize.models.User); // true
+console.log('dd', Partners === sequelize.models.Partners); // true
+console.log('dd', Structures === sequelize.models.Structures); // true
+console.log('dd', Permission === sequelize.models.Permission); // true
+console.log(performance)
